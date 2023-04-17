3b260b12bbf3f6a92393f905515416b83a63e9f8
D1406 <dy8388388@gmail.com>
2023-04-17 18:25:04 +0300

    window title ready

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -20,8 +20,13 @@ public class Application implements Consumer<Event> {
         window = App.makeWindow();
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
+        // задаём размер окна
+        window.setWindowSize(900, 900);
+        // задаём его положение
+        window.setWindowPosition(100, 100);
         // делаем окно видимым
         window.setVisible(true);
+        window.setTitle("Java 2D");
     }
 
     /**
